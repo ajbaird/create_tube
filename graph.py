751fad3ec839fa332c77692f34e4c596af352f8c
Austin Baird <ajbaird86@gmail.com>
2015-06-15 15:13:59 -0400

    fixing a few things

--- graph.py
+++ graph.py
@@ -1,9 +1,9 @@
 import numpy as np 
 import matplotlib.pyplot as plt 
 
-array = np.loadtxt('tube_points.txt')
+array = np.loadtxt('tube_points.vertex', skiprows=1)
 
 print np.shape(array)
 
-plt.plot(array[:,0], array[:,1])
+plt.plot(array[:,0], array[:,1],'bD')
 plt.show()
